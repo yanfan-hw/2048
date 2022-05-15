3da78bb2e09ef4d0b6a3d60dd0da8c742f1ad47e
Thong Doan <dmthong181096@gmail.com>
2022-05-15 14:50:26 +0700

    create Scence Main

--- assets/Script/welcome.js
+++ assets/Script/welcome.js
@@ -10,29 +10,21 @@ cc.Class({
             default: null,
             type: cc.Node
         },
-        gameBoard: {
-            default: null,
-            type: cc.Node
-        },
         _text: '2048'
     },
 
     // use this for initialization
     onLoad: function () {
-        this.gameBoard.active = false;
-
+        this.label.string = this._text;
+        this.label.fontSize = 200;
         cc.tween(this.label.node)
             .to(2, { opacity: 0 })
             .call(() => {
-                this.background.color = new cc.Color(255, 255, 255);
-                this.gameBoard.active = true; 
+                cc.director.loadScene("Main")
             })
             .start()
-        this.label.string = this._text;
-        this.label.fontSize = 200;
     },
 
-    // called every frame
     // update: function (dt) {
     // },
 });
