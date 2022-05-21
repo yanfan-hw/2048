127d5d45160d9e09daa70eaaf5fcd69faabae9ed
Thong Doan <dmthong181096@gmail.com>
2022-05-21 13:20:17 +0700

    fix new game

--- assets/Script/bestScore.js
+++ assets/Script/bestScore.js
@@ -14,21 +14,6 @@ cc.Class({
 
     properties: {
         bestScoreNumber: cc.Label
-        // foo: {
-        //     // ATTRIBUTES:
-        //     default: null,        // The default value will be used only when the component attaching
-        //                           // to a node for the first time
-        //     type: cc.SpriteFrame, // optional, default is typeof default
-        //     serializable: true,   // optional, default is true
-        // },
-        // bar: {
-        //     get () {
-        //         return this._bar;
-        //     },
-        //     set (value) {
-        //         this._bar = value;
-        //     }
-        // },
     },
 
     // LIFE-CYCLE CALLBACKS:
